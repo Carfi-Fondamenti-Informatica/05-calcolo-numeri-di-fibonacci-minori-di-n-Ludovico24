7c1032350ecc84a8ee42e606111343d4472637ca
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-11-26 17:32:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+   int a=1, b=1, c, n;
+cin >> n;
+cout << a << endl << b << endl;
+for (int i=0; i<=n; i++) {
+    c= a+b;
+    cout << c << endl;
+    a=b;
+    b=c;
+}     
    return 0;
 }
